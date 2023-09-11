5cba2bbeb786ed8fc6d6a5fee8fe504164ac68c3
Alexandro Jorge <alexandro.jorge@proway.treina>
2023-09-11 13:38:37 -0300

    atualizações

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ var numero_aleatorio = Math.floor(Math.random() * 100) + 1;
 
 function comecar() {
     //jogada do jogador
+    
     var jogada = parseInt(document.getElementById("input").value);
     tentativas++;//somando tentativas
     document.getElementById("tentativas").textContent = "Tentativas:" + tentativas;//colocando tentativas na tela,textContent modifica o texto que ja tava
     document.getElementById("pontos").textContent = "Pontos:" + pontos;//colocando pontos na tela
     if (jogada > numero_aleatorio) {
         document.getElementById("painel").textContent = "Numero muito alto";//numero mais alto
+        jogada.value = " ";
     }
     else if (jogada < numero_aleatorio) {
         document.getElementById("painel").textContent = "Numero muito baixo";//numero mais baixo
+        jogada.value = " ";
     }
-    else {
+    else if (jogada == numero_aleatorio){
         document.getElementById("painel").textContent = "voce ganhou";//voce ganhou
         jogada = parseInt(document.getElementById("input").value);//nova jogada
         pontos++;//somando ponto de vitoria
@@ -30,6 +33,11 @@ function comecar() {
         document.getElementById("pontos").textContent = "Pontos:" + pontos;
         document.getElementById("painel1").textContent = "jogue novamente";
     }
+    else {
+        document.getElementById("painel").textContent = "Digite algo!";//não deixa ganhar sem digitar nada
+        tentativas = 0;//zerar tentativas
+        document.getElementById("tentativas").textContent = "Tentativas:" + tentativas;
+    }
     
 };
 //faz com que a tela não recarregue depois de enviar o forms,muito importante
@@ -39,3 +47,11 @@ forms.addEventListener("submit", function (event) {
     event.preventDefault();
 });
 /////////Fim/////////////////
+function apagarConteudo() {
+    // Obtenha o campo de entrada pelo ID
+    var apagar = document.getElementById("input");
+  
+    // Limpe o conteúdo do campo de entrada definindo o valor como uma string vazia
+    apagar.value = "";
+  }
+  
